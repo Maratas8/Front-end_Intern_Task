364178f2f2f1d7a064bdd9fd015005e111fdfb41
Marius Atgalainis <maratas@gmail.com>
2022-05-12 12:37:12 +0300

    Sorting by name implemented

--- countries/src/components/Button.jsx
+++ countries/src/components/Button.jsx
@@ -4,7 +4,7 @@ function Button({ text, onClick }) {
   return (
     <div>
       <button
-        className="bg-emerald-700 m-2 mt-8 px-4 py-2 rounded-lg hover:bg-emerald-600"
+        className="bg-emerald-700 m-2 mt-8 px-4 py-2 rounded-lg hover:bg-emerald-600 w-52"
         type="button"
         onClick={onClick}
       >
--- countries/src/components/List.jsx
+++ countries/src/components/List.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import ListItem from "./ListItem";
+import Button from "./Button";
 
 function List() {
   const [countries, setCountries] = useState();
   const [loading, setLoading] = useState(true);
+  const [ascending, setAscending] = useState(true);
+  const [sortButtonName, setSortButtonName] = useState("Sort Z to A");
 
   useEffect(() => {
     fetch(`https://restcountries.com/v2/all?fields=name,region,area`)
@@ -14,20 +17,41 @@ function List() {
       });
   }, []);
 
-  console.log(countries);
+  const togleSort = () => {
+    setAscending(!ascending);
+    ascending ? setSortButtonName("Sort A to Z") : setSortButtonName("Sort Z to A");
+  };
+
+  const mapCountries = () => {
+    return countries.map((country, index) => (
+      <ListItem
+        key={index}
+        name={country.name}
+        region={country.region}
+        area={country.area}
+      />
+    ));
+  };
 
   if (!loading) {
     return (
+      <div>
+        <div className="flex justify-between">
+          <div className="flex">
+            <Button text="Smaller than Lithuania" />
+            <Button text="Is in Oceania" />
+          </div>
+          <div>
+            <Button text={sortButtonName} onClick={togleSort} />
+          </div>
+        </div>
         <div className="">
-        {countries.map((country, index) => (
-          <ListItem key={index} name={country.name} region={country.region} area={country.area}/>
-        ))}
+          {ascending ? mapCountries() : mapCountries().reverse()}
+        </div>
       </div>
     );
   } else {
-      return (
-          <div>Loading...</div>
-      )
+    return <div>Loading...</div>;
   }
 }
 
